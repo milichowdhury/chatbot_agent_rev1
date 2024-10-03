aa229a5acf3a48e69846658278e83251b4507b25
milichowdhury <mili.chowdhury_ds24sp@praxistech.school>
2024-10-04 01:56:40 +0530

    Update utils.py

--- src/data-analysis-llm-agent/utils.py
+++ src/data-analysis-llm-agent/utils.py
@@ -3,7 +3,6 @@ import sqlite3
 EXTRA_SCHEMA_INFO = """
 """
 
-
 async def run_db_query(sql_query):
     connection = None
     try:
@@ -34,35 +33,15 @@ async def run_db_query(sql_query):
             print("SQLite connection is closed")
 
 
-def generate_postgres_table_info_query(schema_table_pairs):
-    query = """
-    SELECT
-        cols.table_schema,
-        cols.table_name,
-        cols.column_name,
-        cols.data_type,
-        coalesce(com.description, '') as column_description
-    FROM
-        information_schema.columns cols
-    LEFT JOIN
-        pg_class cl ON cl.relname = cols.table_name
-    LEFT JOIN
-        pg_description com ON com.objoid = cl.oid AND com.objsubid = cols.ordinal_position
-    WHERE
-        (cols.table_schema, cols.table_name) IN ({});
-    """.format(', '.join(["('{}', '{}')".format(schema, table) for schema, table in schema_table_pairs]))
-
-    return query
-
 def generate_sqlite_table_info_query(schema_table_pairs):
- sql_query = """SELECT 
-    sql
-    FROM 
-    sqlite_master m 
-    WHERE 
-    m.type='table' AND m.name NOT LIKE 'sqlite_%';"""
-
- return sql_query
+    sql_query = """SELECT 
+       sql
+       FROM 
+       sqlite_master m 
+       WHERE 
+       m.type='table' AND m.name NOT LIKE 'sqlite_%';"""
+    
+    return sql_query
 
 def format_table_info(results, columns):
     table_info = ""
@@ -94,11 +73,10 @@ def format_table_info(results, columns):
 def format_sample_data(column_names, data_records):
     formatted_data = ""
     for col_name in column_names:
-        # Get unique non-empty values for the column
         values = set(record[column_names.index(col_name)] for record in data_records if record[column_names.index(col_name)] is not None and record[column_names.index(col_name)] != '')
-        if values:  # Check if values exist
+        if values:
             formatted_data += f"{col_name}: "
-            sample_values = ', '.join(str(value) for value in list(values)[:3])  # Display first 3 unique values
+            sample_values = ', '.join(str(value) for value in list(values)[:3])
             if len(values) > 3:
                 sample_values += ", ..."
             formatted_data += sample_values + '\n'
@@ -109,43 +87,33 @@ def generate_sample_data_query(schema, table, N):
     return f"""SELECT * FROM "{schema}"."{table}" ORDER BY RANDOM() LIMIT {N};"""
 
 
-# formatting data
 def convert_to_json1(rows, column_names):
     results = []
-
     for row in rows:
-        # Convert row data into a dictionary with column names as keys
         row_dict = dict(zip(column_names, row))
-        results.append(list(row_dict.values()))  # Append row values as a list
+        results.append(list(row_dict.values()))
 
-    # Construct the JSON data structure
     json_data = {"columns": column_names, "data": results}
     return json_data
 
 
-
-# formatting data
 def convert_to_json(rows, column_names):
     results = []
     for row in rows:
         row_dict = dict(zip(column_names, row))
         results.append(row_dict)
 
-        # Serialize the results and column names into a JSON string
-    json_data ={"columns": column_names, "data": results}
+    json_data = {"columns": column_names, "data": results}
     return json_data
 
 
 def json_to_markdown_table(json_data):
-    # Extract columns and data from JSON
     columns = json_data["columns"]
     data = json_data["data"]
 
-    # Generate Markdown table header
     markdown_table = "| " + " | ".join(columns) + " |\n"
     markdown_table += "| " + " | ".join(["---"] * len(columns)) + " |\n"
 
-    # Generate Markdown table rows
     for row in data:
         markdown_table += "| " + " | ".join(str(row[column]) for column in columns) + " |\n"
 
